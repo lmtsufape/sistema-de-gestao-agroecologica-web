2c556391f0e7495b9c2eacaaa63dd2256ca132b5
Izabel Vidal <izabelvidal99@gmail.com>
2020-09-10 20:04:19 -0300

    terminei por hoje

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Qual é o nome do projeto?</title>
+
         <link rel="stylesheet" href="/styles/login.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
     </head>
     <body>
         <nav>
@@ -13,11 +16,30 @@
         </nav>
 
         <div class= conteudo-principal>
+            <h1>Bem Vindo de volta</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+                Cras pellentesque maximus tempor.
+            </p>
+            <div class="informacoes-login">
+                <div class="caixa-texto">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <input type="email">
+                </div>
 
+                <div class="caixa-texto">
+                    <i class="fa fa-unlock-alt" aria-hidden="true"></i>
+                    <input type="password">
+                </div>
+            </div>
+            
+
+            <button>Login</button>
+            
         </div>
 
         <div class="imagem">
-            <button>Cadastrar</button>
+           <a href="/"><button>Cadastrar</button></a> 
         </div>
     
         <footer>
