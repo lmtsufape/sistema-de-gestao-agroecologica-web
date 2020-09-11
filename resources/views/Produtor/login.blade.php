5932e065d8cacaf9e88bebb9c468121bbad3cedd
Izabel Vidal <izabelvidal99@gmail.com>
2020-09-11 11:34:54 -0300

    erros e mais erros :)

---
+++ resources/views/Produtor/login.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Qual é o nome do projeto?</title>
+
+        <link rel="stylesheet" href="/styles/login.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    </head>
+    <body>
+        <nav>
+            <div class=logo>logo</div>
+        </nav>
+
+        <div class= conteudo-principal>
+            <h1>Bem Vindo de volta</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+                Cras pellentesque maximus tempor.
+            </p>
+            <div class="informacoes-login">
+                <div class="caixa-texto">
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <input type="email">
+                </div>
+
+                <div class="caixa-texto">
+                    <i class="fa fa-unlock-alt" aria-hidden="true"></i>
+                    <input type="password">
+                </div>
+            </div>
+    
+            <a href="/">
+            <button>Login</button>
+            </a>
+        </div>
+
+        <div class="imagem">
+           <a href="/"><button>Cadastrar</button></a> 
+        </div>
+    
+        <footer>
+            isso aqui é um rodapé
+        </footer>
+    </body>
+</html>
--- resources/views/incluir/footer.blade.php
+++ resources/views/incluir/footer.blade.php
@@ -1,2 +1,11 @@
 <footer>
+    <div class="container">
+        <div class="logo-ufape">
+            <img src="../images/ufape.png" alt="">
+        </div>
+
+        <div class="logo-lmts">
+            <img src="../images/lmts.png" alt="">
+        </div>
+    </div>
 </footer>
