199e0ceceaf99f22efcf55adf78c7bb1631ca269
Carlos Antônio <carlosaajunior.jp@gmail.com>
2020-09-15 07:06:00 -0300

    Models iniciais feitos

---
+++ app/Models/Ocs.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Ocs extends Authenticatable
+{
+
+
+}
