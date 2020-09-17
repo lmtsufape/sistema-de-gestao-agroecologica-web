205ded64152b315baa2bf462e6a28985b90ead18
Izabel Vidal <izabelvidal99@gmail.com>
2020-09-17 09:15:38 -0300

    footer flexbox

--- resources/views/incluir/footer.blade.php
+++ resources/views/incluir/footer.blade.php
@@ -6,6 +6,13 @@
         <div class="logo-lmts">
             <img src="../images/lmts.png" alt="">
         </div>
+        <div class="mapa-site">
+            <h5>Não sei o que to fazendo</h5>
+            <p>eu ai</p>
+            <p>eu ai</p>
+            <p>eu ai</p>
+            <p>eu ai</p>
+        </div>
     </div>
     
 </footer>
