ff9c90fdf7365d953e21fd08e25e6db1e8910649
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-02 09:53:05 -0300

    não confio na minha máquina

--- resources/views/incluir/footer.blade.php
+++ resources/views/incluir/footer.blade.php
@@ -1,15 +1,17 @@
 <footer>
     <div class="container-fluid rodape" aling="center">
-        <div class="container py-5 partedecima">
-            Ínico • SGA • Sobre
+        <div class="container py-5">
+            <p class="texto-footer">
+              Ínicio • SGA • Sobre  
+            </p>
         </div>
         <div class="container">
             <div class="row">
                 <div class="col-sm">
-                    <img src="{{ asset('images/lmts.png') }}" alt="">
+                    <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/lmts.png') }}" alt="">
                 </div>
                 <div class="col-sm">
-                    <img src="{{ asset('images/ufape.png') }}" alt="">
+                    <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/ufape.png') }}" alt="">
                 </div>
                 <div class="col-sm">
                     Rede Sociais:
