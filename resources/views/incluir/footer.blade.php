7c85cd976d0f5e1f0539809cea4bc6248b2b075f
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-02 10:26:26 -0300

    footer quase pronto

--- resources/views/incluir/footer.blade.php
+++ resources/views/incluir/footer.blade.php
@@ -5,16 +5,29 @@
               Ínicio • SGA • Sobre  
             </p>
         </div>
-        <div class="container">
+        <div class="container py-4">
             <div class="row">
+                Desenvolvido por:
                 <div class="col-sm">
                     <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/lmts.png') }}" alt="">
                 </div>
+                Apoio:
                 <div class="col-sm">
                     <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/ufape.png') }}" alt="">
                 </div>
                 <div class="col-sm">
                     Rede Sociais:
+                    <div class="row">
+                        <div class="col-sm">
+                            <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/facebook.png') }}" alt="">
+                        </div>
+                        <div class="col-sm">
+                            <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/instagram.png') }}" alt="">
+                        </div>
+                        <div class="col-sm">
+                            <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/twitter.png') }}" alt="">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/incluir/navbar.blade.php
+++ resources/views/incluir/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark navegação">
+<nav class="navbar navbar-expand-md navbar-dark navegação mb-5">
     <a class="navbar-brand mb-0 h1" href="#">Sistema de Gestão Agrícola</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
