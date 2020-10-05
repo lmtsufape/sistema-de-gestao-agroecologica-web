a8e16c242f06170e04f82ac8af627d49a376055e
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-05 09:57:54 -0300

    navbar pronto

--- resources/views/incluir/footer.blade.php
+++ resources/views/incluir/footer.blade.php
@@ -1,31 +1,42 @@
 <footer>
-    <div class="container-fluid rodape" aling="center">
-        <div class="container py-5">
+    <div class="container-fluid rodape mt-5" aling="center">
+        <div class="container pt-5">
             <p class="texto-footer">
               Ínicio • SGA • Sobre  
             </p>
         </div>
-        <div class="container py-4">
-            <div class="row">
+        <div class="container py-4 border-top border-light">
+            <div class="row py-2">
                 Desenvolvido por:
                 <div class="col-sm">
-                    <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/lmts.png') }}" alt="">
+                    <a href="http://lmts.uag.ufrpe.br/" target="_blank">
+                        <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/lmts.png') }}" alt="">
+                    </a>
                 </div>
                 Apoio:
                 <div class="col-sm">
-                    <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/ufape.png') }}" alt="">
+                    <a href="http://ufape.edu.br/" target="_blank">
+                        <img class="img-thumbnail rounded float-left imagem" src="{{ asset('images/ufape.png') }}" alt="">
+                    </a>
                 </div>
                 <div class="col-sm">
                     Rede Sociais:
                     <div class="row">
                         <div class="col-sm">
-                            <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/facebook.png') }}" alt="">
+                            <a href="https://www.facebook.com/LMTSUFAPE/" target="_blank">
+                                <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/facebook.png') }}" alt="">
+                            </a>
                         </div>
                         <div class="col-sm">
-                            <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/instagram.png') }}" alt="">
+                            <a href="https://www.instagram.com/lmts_ufape/" target="_blank">
+                                <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/instagram.png') }}" alt="">
+                            </a>
                         </div>
                         <div class="col-sm">
-                            <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/twitter.png') }}" alt="">
+                            <a href="" target="_blank">
+                                <img class="img-thumbnail my-5 imagem redesocial" src="{{ asset('images/twitter.png') }}" alt="">
+                            </a>
+                            
                         </div>
                     </div>
                 </div>
--- resources/views/incluir/navbar.blade.php
+++ resources/views/incluir/navbar.blade.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-md navbar-dark navegação mb-5">
-    <a class="navbar-brand mb-0 h1" href="#">Sistema de Gestão Agrícola</a>
+    <a class="navbar-brand mb-0 h1" href="#"><img class="img-thumbnail imagem" src="{{ asset('images/logo2.png') }}" alt="Logo Sistema de Gestão Agricola"> Sistema de Gestão Agrícola</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown mr-5">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Menu
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Minhas Informações</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">log out</a>
-                </div>
-            </li>
+            @guest
+            <li class="nav-item">
+                @if(Route::currentRouteName() == 'register')
+                    <a class="nav-link" href="{{ route('login') }}"><button type="button" class="btn btn-outline-light botão">Login</button></a>
+                @else
+                    <a class="nav-link" href="{{ route('register') }}"><button type="button" class="btn btn-outline-light botão">Cadastrar</button></a>
+                @endif
+                </li>
+            @endauth
         </ul>
     </div>
 </nav>
+
+<!-- A IMAGEM USADA É SÓ UM EXEMPLO, LOGO AINDA EM ANDAMENTO"-->
