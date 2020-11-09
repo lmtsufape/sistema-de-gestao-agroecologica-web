a3047ecf73ffe784776ed3c3c3fe744147c35033
Carlos Antônio <carlosaajunior.jp@gmail.com>
2020-11-09 04:53:02 -0300

    Adicionando as funções de listagem de produtores da OCS

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
