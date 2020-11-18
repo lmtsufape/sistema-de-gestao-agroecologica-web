65c2a63977daff7be3e23a3bac445e03507198f3
BASDiniz <brunodiniz1818@gmail.com>
2020-11-18 13:54:53 -0300

    Corrigindo erro - Nome do user no navbar

--- resources/views/incluir/navbar.blade.php
+++ resources/views/incluir/navbar.blade.php
@@ -19,7 +19,7 @@
             @auth
             <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->get()->first()->nome }}
+                    {{ Auth::user()->nome }}
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
