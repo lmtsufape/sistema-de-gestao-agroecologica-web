9a5082f72c1fd448e8bf7a786606e348b4b46862
BASDiniz <brunodiniz1818@gmail.com>
2020-11-25 18:16:15 -0300

    Adicionando link de Inicio  e mudando formato do nome do user no navbar

--- resources/views/incluir/navbar.blade.php
+++ resources/views/incluir/navbar.blade.php
@@ -17,9 +17,13 @@
             @endauth
 
             @auth
+            <li>
+                <a id ="inicio" href="{{route('home')}}" class="nav-link" role="button">Início</a>
+            </li>
+        
             <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->nome }}
+                    Olá, <strong>{{ Auth::user()->nome }}</strong>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
